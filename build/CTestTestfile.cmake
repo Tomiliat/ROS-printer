8718050ae1f7f945cea1b2af3e7e1eaf40627771
tomilia.tsereh <tom.tsereh@gmail.com>
2020-03-10 20:57:45 +0100

    init commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/tomiliatsereh/printertool/src
+# Build directory: /home/tomiliatsereh/printertool/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("printer")
---
+++ build/printer/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tomiliatsereh/printertool/src/printer
+# Build directory: /home/tomiliatsereh/printertool/build/printer
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
